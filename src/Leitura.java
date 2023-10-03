ecea7bfc459a86ddd9ade59237ad8ddfc1896ddb
Fillipe Andrade Barreto <fillipe.barreto@faculdadesjorgeamado.com.br>
2023-10-03 14:04:50 -0300

    leitura de dados

--- src/Leitura.java
+++ src/Leitura.java
@@ -3,6 +3,17 @@ public class Leitura {
 
     public static void main (String[] args){
         Scanner leitor =  new Scanner(System.in);
-
+        System.out.println("Digite seu filme favorito");
+        String favorito;
+        int ano;
+        double avalia;
+        favorito = leitor.nextLine();
+        System.out.println("Digite o ano de lançamento");
+        ano = leitor.nextInt();
+        System.out.println("Digite sua avaliação desse filme até 5 estrelas");
+        avalia = leitor.nextDouble();
+        System.out.println("Nome do filme favorito: "+favorito);
+        System.out.println("Ano de lançamento: "+ano);
+        System.out.println("a avaliação foi de: "+avalia);
     }
 }
